14998fe15e5434ca5236511e7e26334bc97f84bc
Leopoldo Caballero <git@pitakill.net>
2019-11-18 21:22:29 -0600

    Better UI for upload the file
    1. Why is this change necessary?
    Because a better UI means better options for the user of the system
    
    2. How does it address the issue?
    The user can modify the hcl/json file before upload directly from the
    browser
    
    3. What side effects does this change have?
    None

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b47a10db43f768e1a6bab072d3160ec4.js"
+  "/precache-manifest.2e4931d6e890289b4b5c72b32a5e101b.js"
 );
 
 self.addEventListener('message', (event) => {
